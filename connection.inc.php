bdccc75db86e6edccf5ae7e7a75568db5a5e1027
Vishal624 <116059316+Vishal624@users.noreply.github.com>
2026-02-22 23:45:29 +0530

    Initial commit with dynamized secrets

---
+++ connection.inc.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+$db_host = getenv('DB_HOST') ?: "localhost";
+$db_user = getenv('DB_USER') ?: "root";
+$db_pass = getenv('DB_PASS') ?: "";
+$db_name = getenv('DB_NAME') ?: "ecomm";
+
+$con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+mysqli_query($con, "SET SESSION sql_mode=''");
+define('SERVER_PATH', $_SERVER['DOCUMENT_ROOT'] . '/');
+$protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http");
+define('SITE_PATH', $protocol . "://" . $_SERVER['HTTP_HOST'] . "/");
+
+define('INSTAMOJO_REDIRECT', SITE_PATH . 'payment_complete.php');
+
+define('PRODUCT_IMAGE_SERVER_PATH', SERVER_PATH . 'media/product/');
+define('PRODUCT_IMAGE_SITE_PATH', SITE_PATH . 'media/product/');
+
+define('PRODUCT_MULTIPLE_IMAGE_SERVER_PATH', SERVER_PATH . 'media/product_images/');
+define('PRODUCT_MULTIPLE_IMAGE_SITE_PATH', SITE_PATH . 'media/product_images/');
+
+define('BANNER_SERVER_PATH', SERVER_PATH . 'media/banner/');
+define('BANNER_SITE_PATH', SITE_PATH . 'media/banner/');
+
+define('INSTAMOJO_KEY', getenv('INSTAMOJO_KEY') ?: 'key');
+define('INSTAMOJO_TOKEN', getenv('INSTAMOJO_TOKEN') ?: 'token');
+//https://www.youtube.com/watch?v=zWLKQ_loJqI&list=PLWCLxMult9xfYlDRir2OGRZFK397f3Yeb&index=24
+
+
+define('SMTP_EMAIL', getenv('SMTP_EMAIL') ?: 'vishal0786sandhu@gmail.com');
+define('SMTP_PASSWORD', getenv('SMTP_PASSWORD') ?: 'bneauqbbppwvxhdb');
+// https://www.youtube.com/watch?v=aBbmo1pi5B0&list=PLWCLxMult9xfY_dsYicKGcCLhlZ6YXFMh&index=1
+
+
+define('SMS_KEY', getenv('SMS_KEY') ?: 'pVmJMoYOXlLsczICQd6j7qkvFEAiUtg0yDRZ92a13GTw5bHuPhqpeGkdDl5xzvTKVNtcfJ7mMBuRi2Ya');
+//https://www.youtube.com/watch?v=_XaaIJlkNV4&list=PLWCLxMult9xfYlDRir2OGRZFK397f3Yeb&index=27
+?>
